7e1695e642d03ad873f970b79a31eecb0b0ed3bb
Touheed <you@example.com>
2023-04-17 12:52:01 +0530

    db_config

--- libs/db_config.js
+++ libs/db_config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = () => {
     mongoose.set('strictQuery',false);
-    mongoose.connect("mongodb://localhost:27017/Crud_mongodb",{useNewUrlParser: true},function(err,res){
+    mongoose.connect("mongodb+srv://colandev0:feLKkbcZuIuAmeX7@colan-dev.is7xj5p.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser: true},function(err,res){
         if(err){
             throw err;
            console.log("Not Connected",err);
